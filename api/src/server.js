fffc9ff680f3b646e872f9f89881065ab58fa77a
Harshit-Thakur21 <harshitthakur2102@gmail.com>
2026-03-17 23:24:48 +0530

    db and tools setup

--- api/src/server.js
+++ api/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import {ENV} from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
@@ -23,4 +24,18 @@ if(ENV.NODE_ENV === "production"){
     })
 }
 
-app.listen(ENV.PORT, () => console.log("Server is runnig on port:", ENV.PORT));
+
+const startServer = async () => {
+    try {
+        connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log("Server is runnig on port:", ENV.PORT)
+            connectDB();
+        });
+    }
+    catch(error) {
+        console.error("❌ Error starting the server", error);
+    }
+}
+
+startServer();
