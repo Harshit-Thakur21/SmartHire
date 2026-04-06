55635fe666044daf5c271523db221fb6ca391930
Harshit-Thakur21 <harshitthakur2102@gmail.com>
2026-04-06 16:02:52 +0530

    hari om

--- api/src/lib/inngest.js
+++ api/src/lib/inngest.js
@@ -1,37 +1,34 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
-import { ENV } from "./env.js";
 
-export const inngest = new Inngest({ id: "smart-hire", eventKey: ENV.INNGEST_EVENT_KEY, });
+export const inngest = new Inngest({ id: "smart-hire" });
 
 const syncUser = inngest.createFunction(
-  { id: "sync-user-v2" },
-  { event: "clerk/user.created" },
+  { 
+    id: "sync-user-v2",
+    triggers: [{ event: "clerk/user.created" }]  // ← array inside first arg
+  },
   async ({ event }) => {
     await connectDB();
-
     const { id, email_addresses, first_name, last_name, image_url } = event.data;
-
-    const newUser = {
+    await User.create({
       clerkId: id,
       email: email_addresses[0]?.email_address,
-      name: `${first_name || ""} ${last_name || ""}`,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
       profileImage: image_url,
-    };
-
-    await User.create(newUser);
+    });
   }
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user-from-db-v2" },
-  { event: "clerk/user.deleted" },
+  { 
+    id: "delete-user-from-db-v2",
+    triggers: [{ event: "clerk/user.deleted" }]  // ← array inside first arg
+  },
   async ({ event }) => {
     await connectDB();
-
-    const { id } = event.data;
-    await User.deleteOne({ clerkId: id });
+    await User.deleteOne({ clerkId: event.data.id });
   }
 );
 
