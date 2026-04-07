ab2424ddcf4ed7423c97ae68c4133ba8ae4899f0
Harshit-Thakur21 <harshitthakur2102@gmail.com>
2026-04-08 00:17:54 +0530

    frontend-tools-setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/react'
+import { BrowserRouter } from 'react-router'
+import { 
+  QueryClient,
+  QueryClientProvider,
+ } from '@tanstack/react-query'
+
+//Import your Publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key. Please add it to your .env file')
+}
+
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider>
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
+    </QueryClientProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,22 @@
+import { Show, SignInButton, SignOutButton, SignUpButton, UserButton } from '@clerk/react'
+import toast from 'react-hot-toast'
+
+function HomePage() {
+  return (
+    <div>
+        <button className='btn' onClick={() => toast.error("This is a success toast")}>Click me</button>
+         <Show when="signed-out">
+          <SignInButton mode='modal'>
+            <button>Login</button>
+          </SignInButton>
+        </Show>
+        <Show when="signed-in">
+          <SignOutButton />
+        </Show>
+
+        <UserButton />
+    </div>
+  )
+}
+
+export default HomePage
